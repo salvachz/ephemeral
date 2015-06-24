83eac70435279b1720913996481d9606ab9ca4ad
Salvador <salvachz@gmail.com>
2015-06-24 15:40:54 -0300

    top produtos business

--- ephemeral/apps/business/views/businessTopProduct.py
+++ ephemeral/apps/business/views/businessTopProduct.py
@@ -6,6 +6,13 @@ class BusinessTopProductView(EphemeralTemplateView):
     template_name = "business/businessTopProduct.html"
 
     def get(self, request, **kwargs):
-        topProducts = []
-        
+        kwargs['topProducts'] = []
+        products = ProdutoPedido.objects.values('produto_id').distinct()
+        for product_row in products:
+            data = {}
+            product_id = product_row['produto_id']
+            data['product'] = Produto.objects.get(id=product_id)
+            data['orders'] = ProdutoPedido.objects.filter(produto_id=product_id).count()
+            data['sumQnt'] = ProdutoPedido.objects.filter(produto_id=product_id).aggregate(Sum('quantidade'))
+            kwargs['topProducts'].append(data)
         return EphemeralTemplateView.get(self, request,**kwargs)
