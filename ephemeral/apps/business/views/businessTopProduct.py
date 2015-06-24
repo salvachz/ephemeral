bcb90d60954a78a5f2c607e2609e3b5f93c51688
Salvador <salvachz@gmail.com>
2015-06-24 14:55:23 -0300

    bug fix

--- ephemeral/apps/business/views/businessTopProduct.py
+++ ephemeral/apps/business/views/businessTopProduct.py
@@ -1,11 +1,11 @@
 from apps.generic.views import EphemeralTemplateView
 from django.http import HttpResponseRedirect
-from apps.generic.models import Pedido, Produto, PedidoProduto
+from apps.generic.models import Pedido, Produto, ProdutoPedido
 
 class BusinessTopProductView(EphemeralTemplateView):
     template_name = "business/businessTopProduct.html"
 
     def get(self, request, **kwargs):
         topProducts = []
-        distinct_products = PedidoProduto.objects
+        
         return EphemeralTemplateView.get(self, request,**kwargs)
